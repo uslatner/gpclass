1552f1527548cbabf66a1fc13f62f8f641d84644
uslatner <ursa.slatner@gmail.com>
2024-04-24 09:35:55 +1000

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import './scss/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/gpclass">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
